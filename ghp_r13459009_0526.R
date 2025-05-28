4e528f89d34789495a156a17d6cfc28e90a0d370
yuisft <r13459009@ntu.edu.tw>
2025-05-28 20:56:47 +0800

    Update ghp_r13459009_0526.R

--- ghp_r13459009_0526.R
+++ ghp_r13459009_0526.R
@@ -1,16 +1,17 @@
-# 使用パッケージ
+# used package
 library(dplyr)
 
-# ファイル読み込み
+# file loading
 data <- read.csv("C:/Users/81806/Documents/ghp_r13459009-5.26/ghp_r13459009_0526/dengue_assignment.csv")
 
-# "year" 列が 2014 の行だけ抽出
+# Extract rows where the 'year' column is 2014
 data_2014 <- filter(data, year == 2014)
 
-# "total_cases" 列の合計を計算
+# Calculate the sum of the 'total_cases' column
 total_cases_2014 <- data_2014 %>% 
   summarise(total = sum(case_number, na.rm = TRUE)) %>% 
   pull(total)
-# 結果を表示
+
+# Display the result
 print(paste("total cases in 2014: ", total_cases_2014))
 
